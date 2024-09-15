f24fc1251267f40a8b1c42fc339f3092f3c063ac
taewoo-dev <twin9774@gmail.com>
2024-09-16 00:35:54 +0900

    💡 chore: django secret_key 수정

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = "django-insecure-ymr=3u3(zk*7e098o!y@m($@q*kks6$^zi2&guz!tl*uwi-1%n"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
